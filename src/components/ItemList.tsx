4be0dca33d77ab71c4b5b5a47a83a791ed907ac3
Audran2 <massacryaudran@gmail.com>
2024-02-15 09:37:44 +0100

    first commit

---
+++ src/components/ItemList.tsx
@@ -0,0 +1,5 @@
+const ItemList = () => {
+  return <div>test</div>;
+};
+
+export default ItemList;
---
+++ src/components/ItemScreen.tsx
@@ -0,0 +1,5 @@
+const ItemScreen = () => {
+  return <div>test</div>;
+};
+
+export default ItemScreen;
